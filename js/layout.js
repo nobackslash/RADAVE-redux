54ade3c21bf08eb6243e9f60a922066c9dd0db60
nobackslash <pedrostraub@outlook.com>
2025-11-10 19:54:02 -0300

    hotfixed header and footer 404

--- js/layout.js
+++ js/layout.js
@@ -1,9 +1,9 @@
 async function loadLayout() {
   // time saving
-  const header = await fetch("/components/header.html").then((res) =>
+  const header = await fetch("../components/header.html").then((res) =>
     res.text()
   );
-  const footer = await fetch("/components/footer.html").then((res) =>
+  const footer = await fetch("../components/footer.html").then((res) =>
     res.text()
   );
 
