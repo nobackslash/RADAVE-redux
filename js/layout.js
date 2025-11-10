2a5089d7bd3a64870322cad30a4c771b833260a8
nobackslash <pdrstrb@gmail.com>
2025-11-09 23:29:58 -0300

    Refactor project structure

---
+++ js/layout.js
@@ -0,0 +1,13 @@
+async function loadLayout() {
+    const header = await fetch("components/header.html").then((res) =>
+        res.text()
+    );
+    const footer = await fetch("components/footer.html").then((res) =>
+        res.text()
+    );
+
+    document.getElementById("header").innerHTML = header;
+    document.getElementById("footer").innerHTML = footer;
+}
+
+window.addEventListener("DOMContentLoaded", loadLayout);
--- script/layout.js
+++
@@ -1,9 +0,0 @@
-async function loadLayout() {
-  const header = await fetch("partials/header.html").then((res) => res.text());
-  const footer = await fetch("partials/footer.html").then((res) => res.text());
-
-  document.getElementById("header").innerHTML = header;
-  document.getElementById("footer").innerHTML = footer;
-}
-
-window.addEventListener("DOMContentLoaded", loadLayout);
